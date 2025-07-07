ee3a99647b8da7de84f4f67a36c8afa4ef84cc53
doron <segal.doron@gmail.com>
2025-07-07 14:35:01 +0300

    remove unused code

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,9 +1,6 @@
 import { createBrowserRouter } from 'react-router-dom';
-// import IssuesTable from '../pages/IsuessTable/IssuesTableTableContainer';
+
 import About from '@/pages/About';
-import Table from '@/components/Table/Table';
-import TableTwo from '@/components/Table2/Table2';
-import IsuessTablePage from '@/pages/IsuessTablePage/IsuessTablePage';
 import WelcomPage from '@/pages/WelcomPage/WelcomPage';
 
 
@@ -12,25 +9,9 @@ const router = createBrowserRouter([
     path: '/',
     element: <WelcomPage />,
   },
-  // {
-  //   path: '/',
-  //   element: <IssuesTable />,
-  // },
   {
     path: '/a11y-issue',
     element: <About />,
-  },
-  {
-    path: '/a11y-issue-table',
-    element: <Table />,
-  },
-  {
-    path: '/a11y-issue-table-2',
-    element: <TableTwo />,
-  },
-    {
-    path: '/a11y-issue-table-virtual',
-    element: <IsuessTablePage />,
   },
 ]);
 
